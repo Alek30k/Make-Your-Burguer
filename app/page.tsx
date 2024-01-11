586f2fc3ace4ad6d45adc47357ca036e918d580d
Alek30k <alemank23k@gmail.com>
2024-01-11 17:10:00 -0300

    last commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import { Button } from "@/components/ui/button";
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-center">HomePage</h1>
-      <Button variant={"outline"}>Button</Button>
+      <h1 className="text-gray-800 text-5xl font-medium ">Make Your Burger</h1>
     </div>
   );
 };
