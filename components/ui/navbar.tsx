d985b486ee2347f25257a5c7bdfe88e66846a51a
Alek30k <alemank23k@gmail.com>
2024-01-11 17:03:22 -0300

    last commit

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,22 +1,54 @@
 import { Phone } from "lucide-react";
 import { Button } from "./button";
 import IconLogo from "./icons/icon-logo";
+import Iconmenu from "./icons/icon.menu";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const Navbar = () => {
   return (
-    <div>
-      <Button
-        variant="outline"
-        size="icon"
-        className="rounded-full bg-gray-100"
-      >
-        <IconLogo />
-      </Button>
-      <Button>
-        <Phone className="mr-2 h-4 w-4" />
-        Call Me Back
-      </Button>
-    </div>
+    <>
+      <nav className="flex justify-between">
+        <Button
+          variant="outline"
+          size="icon"
+          className="rounded-full bg-gray-100"
+        >
+          <IconLogo />
+        </Button>
+        <Button className="rounded-full bg-rose-50 text-red-500 hover:bg-rose-500 hover:text-red-50">
+          <Phone className="mr-2 h-4 w-4" />
+          Call Me Back
+        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="rounded-full bg-gray-100"
+            >
+              <Iconmenu />
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Are you absolutely sure?</SheetTitle>
+              <SheetDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </nav>
+    </>
   );
 };
 
