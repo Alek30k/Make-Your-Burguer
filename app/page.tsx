e733cf6f3d21d4f4f1fee70abaac2784c1a3b0bc
Alek30k <alemank23k@gmail.com>
2024-01-11 18:28:36 -0300

    last commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-gray-800 text-5xl font-medium ">Make Your Burger</h1>
+      <h1 className="text-gray-800 text-5xl font-medium">Make Your Burger</h1>
       <p className="text-gray-800  text-xl font-normal leading-loose">
         Parallax screen. Burger ingredients and emojis moving depending on the
         position of the mouse pointer.
