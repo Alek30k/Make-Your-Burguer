2c6a9b82a8a41e8d5a6668bc326719fcdbb26b98
Alek30k <alemank23k@gmail.com>
2024-01-11 17:17:01 -0300

    last commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,10 @@ const HomePage = () => {
   return (
     <div>
       <h1 className="text-gray-800 text-5xl font-medium ">Make Your Burger</h1>
+      <p className="text-gray-800  text-xl font-normal leading-loose">
+        Parallax screen. Burger ingredients and emojis moving depending on the
+        position of the mouse pointer.
+      </p>
     </div>
   );
 };
