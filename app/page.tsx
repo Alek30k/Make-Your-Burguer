681374dee6b57bb3ec02c4a83d0d2eaa59bd3f61
Alek30k <alemank23k@gmail.com>
2024-01-12 15:23:09 -0300

    last commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import HeroMain from "@/components/ui/hero-main";
 
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-gray-800 text-5xl font-medium mt-5">
-        Make Your Burger
-      </h1>
-      <p className="text-gray-800  text-xl font-normal leading-loose">
-        Parallax screen. Burger ingredients and emojis moving depending on the
-        position of the mouse pointer.
-      </p>
-      <div className="">
-        <Button className="w-32 h-32 rounded-full uppercase text-xl">
-          make burger
-        </Button>
-        <Image
-          src="/images/burger.png"
-          width={448}
-          height={511}
-          alt="Burguer"
-          className="mx-auto"
-        />
-      </div>
+      <HeroMain />
     </div>
   );
 };
---
+++ components/ui/hero-main.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Button } from "./button";
+import Image from "next/image";
+
+const HeroMain = () => {
+  return (
+    <div>
+      <h1 className="text-gray-800 text-5xl font-medium mt-5">
+        Make Your Burger
+      </h1>
+      <p className="text-gray-800  text-xl font-normal leading-loose">
+        Parallax screen. Burger ingredients and emojis moving depending on the
+        position of the mouse pointer.
+      </p>
+      <div className="relative flex justify-center items-center">
+        <Button className="w-32 h-32 rounded-full uppercase text-xl absolute ">
+          make burger
+        </Button>
+        <Image
+          src="/images/burger.png"
+          width={448}
+          height={511}
+          alt="Burguer"
+          className="mx-auto"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HeroMain;
