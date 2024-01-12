36e1b0b454e7e9fe437ef11f371b52179317ffbe
Alek30k <alemank23k@gmail.com>
2024-01-12 15:10:31 -0300

    last commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,28 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-gray-800 text-5xl font-medium">Make Your Burger</h1>
+      <h1 className="text-gray-800 text-5xl font-medium mt-5">
+        Make Your Burger
+      </h1>
       <p className="text-gray-800  text-xl font-normal leading-loose">
         Parallax screen. Burger ingredients and emojis moving depending on the
         position of the mouse pointer.
       </p>
+      <div className="">
+        <Button className="w-32 h-32 rounded-full uppercase text-xl">
+          make burger
+        </Button>
+        <Image
+          src="/images/burger.png"
+          width={448}
+          height={511}
+          alt="Burguer"
+          className="mx-auto"
+        />
+      </div>
     </div>
   );
 };
