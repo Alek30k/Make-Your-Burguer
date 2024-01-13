94cfb1cda525007acfc092e777f83e1774307f30
Alek30k <alemank23k@gmail.com>
2024-01-13 15:06:40 -0300

    last commit

--- components/cart/cart-header.tsx
+++ components/cart/cart-header.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/lib/utils";
+
 interface Props {
   text?: string;
   icon: React.ElementType;
@@ -5,14 +7,16 @@ interface Props {
 }
 
 const CartHeader = ({ text, icon: Icon, iconColor }: Props) => {
-  const baseIconColor =
+  const baseBgIconColor =
     "p-2 h-8 w-8 bg-purple-500 inline-block rounded-full justify-center items-center inline-flex";
+
+  const baseIconColor = "fill-white stroke-purple-500";
 
   return (
     <div>
       <article className="flex gap-x-2 items-center">
-        <span className="">
-          <Icon className="fill-white stroke-purple-500" />
+        <span className={cn(baseBgIconColor, `bg-${iconColor}-600`)}>
+          <Icon className={cn(baseIconColor, `stroke-${iconColor}-600`)} />
         </span>
         <span className="text-gray-800 leading-tight font-semibold">
           {text}
