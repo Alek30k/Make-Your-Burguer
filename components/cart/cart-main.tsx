d1769531a3649075767d2c9929f57f56d76cbe0f
Alek30k <alemank23k@gmail.com>
2024-01-15 16:15:42 -0300

    last commit

--- components/cart/cart-main.tsx
+++ components/cart/cart-main.tsx
@@ -19,8 +19,10 @@ const CartMain = () => {
         <CartHeader text="60 oz" icon={Beaker} iconColor="orange" />
         <CartHeader text="249 kcal" icon={Clock} iconColor="red" />
       </section>
-      <Button className="group bg-rose-50 text-red-500 text-base font-normal w-full  items-center gap-2.5 rounded-2xl mt-8 hover:bg-rose-500 hover:text-red-50">
-        <span>+ Tomato Ketchup</span>
+      <Button className="group bg-rose-50  text-base font-normal w-full  items-center gap-2.5 rounded-2xl mt-8 hover:bg-rose-500 ">
+        <span className="text-red-500 group-hover:text-red-50">
+          + Tomato Ketchup
+        </span>
         <Badge className="py-1.5 bg-red-500 group-hover:bg-rose-50 group-hover:text-red-500 ">
           0.2 oz
         </Badge>
