81839b4b37c0b99ed3bf72d093b9a49b0b562f63
Alek30k <alemank23k@gmail.com>
2024-01-19 15:21:00 -0300

    last commit

--- components/hero-main.tsx
+++ components/hero-main.tsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 const HeroMain = () => {
   return (
     <div className="mt-20 sm:mt-0  sm:items-center sm:max-h-[735px] justify-start gap-[120px] sm:inline-flex">
-      <h1 className="text-gray-800 text-5xl sm:text-7xl font-bold mt-5 sm:leading-[80px] w-[245px]">
+      <h1 className="text-gray-800 text-5xl sm:text-7xl font-bold mt-5 sm:leading-[80px]  sm:w-1/3">
         Make Your Burger
       </h1>
       <p className="sm:hidden text-gray-800  text-xl font-normal leading-loose mt-5">
         Parallax screen. Burger ingredients and emojis moving depending on the
         position of the mouse pointer.
       </p>
-      <div className="relative flex justify-center items-center">
+      <div className="relative flex justify-center items-center  sm:w-2/3">
         <Button className="w-32 h-32 rounded-full uppercase text-xl absolute ">
           make burger
         </Button>
