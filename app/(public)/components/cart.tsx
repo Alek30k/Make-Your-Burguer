8f64254434291c43c1d60bbefb5b0cef1cf3dd4f
Alek30k <alemank23k@gmail.com>
2024-01-22 16:46:06 -0300

    last commit

--- app/(public)/components/cart.tsx
+++ app/(public)/components/cart.tsx
@@ -1,13 +1,14 @@
-import CartDetails from "@/components/cart/cartDetails";
+import CartHeader from "@/components/cart/cart-header";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import { Beaker, Clock } from "lucide-react";
 
 const MakeCart = () => {
   return (
     <div>
       <h2 className="text-gray-800 text-4xl font-semibold mb-4">summary</h2>
       <Separator className="mb-4" />
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-2 mb-4">
         <p className="w-[197px] text-indigo-700 text-4xl font-extrabold ">
           $12.31
         </p>
@@ -15,8 +16,10 @@ const MakeCart = () => {
           Checkout
         </Button>
       </div>
-      <section className="bg-indigo-100 p-1 rounded-full">
-        <CartDetails />
+      <section className="flex justify-between mt-8 bg-violet-50 rounded-[32px] p-4">
+        <CartHeader text="7 min" icon={Clock} iconColor="purple" />
+        <CartHeader text="60 oz" icon={Beaker} iconColor="orange" />
+        <CartHeader text="249 kcal" icon={Clock} iconColor="red" />
       </section>
     </div>
   );
