bc8611b8e79f9df860e9c4e9e7fdf71dadd8364e
Alek30k <alemank23k@gmail.com>
2024-01-24 10:11:39 -0300

    last commit

--- app/(public)/make/components/burger.tsx
+++ app/(public)/make/components/burger.tsx
@@ -8,15 +8,19 @@ interface Props {
 const MakeBurger = ({ ingredients }: Props) => {
   return (
     <div className="relative bg-[url('/images/make/bg.png')] h-[595px] w-full bg-contain bg-no-repeat bg-center">
-      {ingredients.map(({ image, alt }) => (
-        <Image
-          src={image}
-          alt={alt}
-          width={1000}
-          height={300}
-          className="absolute bottom-0"
-        />
-      ))}
+      {ingredients.map(
+        ({ image, alt, id, name, price, quantity, rotate }) =>
+          quantity > 0 && (
+            <Image
+              key={id}
+              src={image}
+              alt={alt}
+              width={1000}
+              height={300}
+              className="absolute bottom-0"
+            />
+          )
+      )}
     </div>
   );
 };
